f801f5ddbcf372c3bb8d94cc94b3db2d6dec2071
aorji <anastasia.orji@gmail.com>
2018-08-16 15:36:06 +0300

    iphone 8 layout

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Anastasiia ORJI on 8/16/18.
+//  Copyright © 2018 Anastasiia ORJI. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    var audioPlayer : AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func notePressed(_ sender: UIButton) {
+    
+        playSound(soundArray[sender.tag - 1])
+        
+    }
+
+    func playSound(_ soundFileName: String) {
+        
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
+    }
+
+}
+
